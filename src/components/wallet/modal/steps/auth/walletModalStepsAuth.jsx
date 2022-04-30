31c310354bc08b4c4db6f10897562ae7eb47308f
Rasmus Lukas <rasmus.lukaas@gmail.com>
2022-04-30 17:06:07 +0300

    - removed extra info from modal auth loading

--- src/components/wallet/modal/steps/auth/walletModalStepsAuth.jsx
+++ src/components/wallet/modal/steps/auth/walletModalStepsAuth.jsx
@@ -12,7 +12,7 @@ export function WalletModalStepsAuth({ closeModal }) {
     >
       <div
         className={
-          "w-4/5 tablet:w-3/4 laptop:w-1/3 h-96 bg-dark10 text-light text-2xl rounded-2xl fixed"
+          "w-4/5 tablet:w-3/4 laptop:w-1/3 h-36 bg-dark10 text-light text-2xl rounded-2xl fixed"
         }
         style={{
           zIndex: "100",
@@ -37,34 +37,13 @@ export function WalletModalStepsAuth({ closeModal }) {
           />
           <div
             className={
-              "rounded-lg h-24 bg-dark9 ml-4 mr-4 mt-12 mb-2 flex justify-center items-center"
-            }
-          >
-            <h4 className={"text-light text-center text-sm p-12"}>
-              By connecting a wallet, you agree to DockerizeIT’s{" "}
-              <span className={"text-accent underline"}>Terms of Service.</span>
-            </h4>
-          </div>
-          <div
-            className={
-              "rounded-lg h-16 bg-dark7 ml-4 mr-4 mt-2 mb-2 flex justify-start items-center"
+              "rounded-lg h-16 bg-dark7 ml-4 mr-4 mt-12 flex justify-start items-center"
             }
           >
             <CgSpinner className={"animate-spin h-8 w-8 ml-4 mr-2"} />
             <h2 className={"text-light font-semibold text-lg"}>
               Initializing ...
             </h2>
-            {/* animate-spin h-5 w-5 */}
-          </div>
-          <div
-            className={
-              "rounded-lg h-32 bg-dark9 ml-4 mr-4 mt-2 mb-2 flex justify-start items-center"
-            }
-          >
-            <p className={"font-semibold text-2xl ml-8"}>MetaMask</p>
-            <p className={"font-normal text-lg ml-8"}>
-              Easy-to-use browser extension.
-            </p>
           </div>
         </div>
       </div>
