ddace74a2e457ecd217d0535a0e4fdf24c3fc1be
Rasmus Lukas <rasmus.lukaas@gmail.com>
2022-05-10 15:55:27 +0300

    - footer at bottom of page

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { FooterHeader } from "./header/footerHeader";
-import { FooterItems } from "./items/footerItems";
-import { FooterSocials } from "./socials/footerSocials";
 import { FooterBottom } from "./bottom/footerBottom";
 
 export function Footer() {
   return (
-    <footer className="bg-dark8 text-center laptop:text-left w-screen">
+    <div className="bg-dark8 text-center laptop:text-left w-screen relative">
       <FooterHeader />
       <FooterBottom />
-    </footer>
+    </div>
   );
 }
--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { MenuDefault } from "../menu/default/menuDefault";
 import { Footer } from "../footer/footer";
 import { MenuDefaultMobile } from "../menu/default/mobile/menuDefaultMobile";
@@ -7,14 +7,14 @@ export function Layout(props) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => {
-      setIsOpen(!isOpen);
-  }
+    setIsOpen(!isOpen);
+  };
 
   return (
     <React.Fragment>
       <MenuDefault isOpen={isOpen} toggle={toggle} />
       <MenuDefaultMobile isOpen={isOpen} />
-      <div className={"bg-light"}>{props.children}</div>
+      <div className={"bg-light min-h-screen"}>{props.children}</div>
       <Footer />
     </React.Fragment>
   );
