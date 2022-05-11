4d5d2d0c81c1d2195d580bfec18ed944a94726ef
Rasmus Lukas <rasmus.lukaas@gmail.com>
2022-05-11 12:35:08 +0300

    - discord link updated

--- src/components/section/social/items/sectionSocialItems.jsx
+++ src/components/section/social/items/sectionSocialItems.jsx
@@ -3,24 +3,24 @@ export const SectionSocialItems=[
         title: "Join our Discord",
         description: "Keep in touch with the community for news and updates.",
         icon: "/icons/discord.png",
-        url: "https://discord.com/"
+        url: "https://discord.gg/gJqfYm7VqC"
     },
     {
         title: "Join our Discord",
         description: "Keep in touch with the community for news and updates.",
         icon: "/icons/discord.png",
-        url: "https://discord.com/"
+        url: "https://discord.gg/gJqfYm7VqC"
     },
     {
         title: "Join our Discord",
         description: "Keep in touch with the community for news and updates.",
         icon: "/icons/discord.png",
-        url: "https://discord.com/"
+        url: "https://discord.gg/gJqfYm7VqC"
     },
     {
         title: "Join our Discord",
         description: "Keep in touch with the community for news and updates.",
         icon: "/icons/discord.png",
-        url: "https://discord.com/"
+        url: "https://discord.gg/gJqfYm7VqC"
     }
 ]
